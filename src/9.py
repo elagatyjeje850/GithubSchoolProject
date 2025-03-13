ff6f06f3736d294dd28a32c4b26c2d74319153a0
elagatyjeje850 <elagatyjeje850@gmail.com>
2025-03-13 11:07:05 +0000

    Add 9.py

---
+++ src/9.py
@@ -0,0 +1,6 @@
+import random
+def get_random_code(length):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return ''.join([random.choice(alphabet) for _ in range(length)])
+
+print(get_random_code(10)) # output: pfkzlxvjrs
